efa3dd3152e5dcfb5a48a83225497239dedf3602
Francisco Jimenez Cabrera <francisco.jimenez.cabrera@canonical.com>
2020-10-28 17:42:07 +0000

    Parse using tree levels

--- docstring_extractor.py
+++ docstring_extractor.py
@@ -6,6 +6,7 @@ from os.path import basename, splitext
 NODE_TYPES = {
     ast.ClassDef: "Class",
     ast.FunctionDef: "Function",
+    ast.Module: "Module",
 }
 
 
@@ -17,29 +18,62 @@ def parse_docstrings(source):
     no docstring, it gives the first line of the class, funcion or method
     block, and docstring is None.
     """
-    tree = ast.parse(source)
-
-    for node in ast.walk(tree):
-        if isinstance(node, tuple(NODE_TYPES)):
-            docstring = ast.get_docstring(node)
-            lineno = getattr(node, "lineno", None)
-
-            if (
-                node.body
-                and isinstance(node.body[0], ast.Expr)
-                and isinstance(node.body[0].value, ast.Str)
-            ):
-                # lineno attribute of docstring node is where string ends
-                lineno = (
-                    node.body[0].lineno
-                    - len(node.body[0].value.s.splitlines())
-                    + 1
-                )
-
-            yield (node, getattr(node, "name", None), lineno, docstring)
+    return process_node(ast.parse(source))
 
 
-def get_docstrings(source, module=None):
+def process_node(node):
+    """Recursive function to obtain ast nodes"""
+    node_type = NODE_TYPES.get(type(node))
+    docstring = ast.get_docstring(node)
+    lineno = getattr(node, "lineno", 0)
+
+    # Recursion with supported node types
+    children = [
+        process_node(n) for n in node.body if isinstance(n, tuple(NODE_TYPES))
+    ]
+
+    return {
+        "type": node_type,
+        "name": getattr(node, "name", None),
+        "line": lineno,
+        "docstring": docstring if docstring else "",
+        "signature": get_signature(node) if node_type == "Function" else None,
+        "content": children,
+    }
+
+
+def get_signature(node):
+    args = []
+
+    if node.args.args:
+        [args.append([a.col_offset, a.arg]) for a in node.args.args]
+    if node.args.defaults:
+        for a in node.args.defaults:
+            if isinstance(a.value, str):
+                value = f"'{a.value}'"
+            else:
+                value = str(a.value)
+
+            args.append([a.col_offset, "=" + value])
+
+    sorted_args = sorted(args)
+    for i, p in enumerate(sorted_args):
+        if p[1].startswith("="):
+            sorted_args[i - 1][1] += p[1]
+    sorted_args = [k[1] for k in sorted_args if not k[1].startswith("=")]
+
+    if node.args.vararg:
+        sorted_args.append("*" + node.args.vararg)
+    if node.args.kwarg:
+        sorted_args.append("**" + node.args.kwarg)
+
+    if sorted_args:
+        return "(" + ", ".join(sorted_args) + ")"
+
+    return None
+
+
+def get_docstrings(source, module_name=None):
     """Parse Python source code from string and print docstrings.
 
     For each class, method or function and the module, prints a heading with
@@ -54,28 +88,21 @@ def get_docstrings(source, module=None):
     """
     if hasattr(source, "read"):
         filename = getattr(source, "name")
-        module = splitext(basename(filename))[0]
         source = source.read()
+
+        if not module_name:
+            module_name = splitext(basename(filename))[0]
 
     docstrings = parse_docstrings(source)
 
-    result = {"module": module, "content": []}
+    if module_name:
+        docstrings["name"] = module_name
 
-    for node, name, lineno, docstring in docstrings:
-        result["content"].append(
-            {
-                "type": NODE_TYPES.get(type(node)),
-                "name": name,
-                "line": lineno,
-                "docstring": docstring if docstring else "",
-            }
-        )
-
-    return result
+    return docstrings
 
 
 if __name__ == "__main__":
     import sys
 
     with open(sys.argv[1]) as file:
-        get_docstrings(file)
+        print(get_docstrings(file))
